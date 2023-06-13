a3e7e5832631730ec4f0a5429f370848da960352
vivianink1991 <binggan26@126.com>
2023-06-13 10:57:41 +0800

    feat: add security related

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+// const { xss } = require('express-xss-sanitizer')
+const xss = require('xss-clean')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -10,25 +14,38 @@ const userRouter = require('./routes/userRoutes')
 const app = express()
 
 // 1) GLOBAL MIDDLEWARES
+
+// Secure HTTP Headers
+app.use(helmet())
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
 	max: 100,
 	window: 60 * 60 * 1000,
 	message: 'Too many requests from this IP, please try again in an hour!'
 })
-
 app.use('/api', limiter) // affect routes starts with /api
 
+// Body parser, reading data from body into req.body
 // middleware, 解析JSON格式的请求body体，在route handler中可通过req.body访问请求body体。
 // 没有该中间件则req.body=undefined
-app.use(express.json())
+app.use(express.json({ limit: '10kb' }))
 
-// Serve static file
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`))
 
+// Test middleware
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString()
 	next()
