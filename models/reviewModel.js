fda5b281165e46ff36c9f6472c1c41919e9b2971
jiaxin.j.li <jiaxin.j.li@mercedes-benz.com>
2023-09-11 16:43:46 +0800

    feat: calculate rating

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -65,17 +65,36 @@ reviewSchema.statics.calcAverageRatings = async function(tourId) {
 			}
 		}
 	])
-	console.log(stats)
-	await Tour.findByIdAndUpdate(tourId, {
-		ratingsQuantity: stats[0].nRating,
-		ratingsAverage: stats[0].avgRating
-	})
+
+	if (stats.length > 0) {
+		await Tour.findByIdAndUpdate(tourId, {
+			ratingsQuantity: stats[0].nRating,
+			ratingsAverage: stats[0].avgRating
+		})
+	} else {
+		await Tour.findByIdAndUpdate(tourId, {
+			ratingsQuantity: 0,
+			ratingsAverage: 4.5
+		})
+	}
 }
 
 reviewSchema.post('save', function() {
 	// this points to current document
 	// contructor points to model since model created after and can't be referenced now
 	this.constructor.calcAverageRatings(this.tour)
+})
+
+// findByIdAndUpdate, findByIdAndDelete是findOneAndXXX的简便方式
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+	// this points the query object
+	this.r = await this.findOne() // return the document.不能放到post的原因是，post时query已经执行完毕，不能再await了。
+	next()
+})
+
+reviewSchema.post(/^findOneAnd/, async function(next) {
+	// await this.findOne() does not work here, query has already executed now.
+	this.r.constructor.calcAverageRatings(this.r.tour)
 })
 
 const Review = mongoose.model('Review', reviewSchema)
