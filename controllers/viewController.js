11e2e07630a3049db83410e6fa5b6837d576c710
jiaxin.j.li <jiaxin.j.li@mercedes-benz.com>
2023-10-12 17:51:38 +0800

    feat: add tour controller

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -15,7 +15,15 @@ exports.getOverview = CatchAsync(async (req, res, next) => {
 	})
 })
 
-exports.getTour = (req, res) => {
+exports.getTour = async (req, res) => {
+	// 1) Get the data, for the requested tour (including reviews and guides)
+	const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+		path: 'reviews',
+		fields: 'review rating user'
+	})
+	// 2) Build template
+
+	// 3) Render template using data from 1)
 	res.status(200).render('tour', {
 		title: 'The Forest Hiker Tour'
 	})
